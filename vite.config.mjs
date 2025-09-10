47f392c3ef1fcf01d7a075daca09f19f9ced4622
Eduardo Borges <eduardo@cubosis.com.br>
2025-09-10 15:39:37 -0300

    Fix base path for GitHub Pages deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/elarin-landing-page/',
+  base: '/elarin-landing-page-main/',
   plugins: [react()],
   server: {
     port: 5174
