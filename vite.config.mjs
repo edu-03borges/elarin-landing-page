b17129bcc5dc94b21f358d3f3c2303e6c4161f8b
Eduardo Borges <eduardo@cubosis.com.br>
2025-09-10 15:28:18 -0300

    first commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/elarin-landing-page-main/',
   plugins: [react()],
   server: {
     port: 5174
